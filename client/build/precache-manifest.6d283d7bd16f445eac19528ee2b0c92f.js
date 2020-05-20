b7c5ead515a1f4a46447c18cf087b43b432fd965
crisypsodium <christinena.316@gmail.com>
2020-05-19 21:44:18 -0700

    created build folder for heroku deployment

---
+++ client/build/precache-manifest.6d283d7bd16f445eac19528ee2b0c92f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b81fcfe5f8a9c08f83c",
+    "url": "/static/css/main.b81ca259.chunk.css"
+  },
+  {
+    "revision": "3b81fcfe5f8a9c08f83c",
+    "url": "/static/js/main.d697532f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a9da008ff1e9cb92c39c",
+    "url": "/static/css/2.17985334.chunk.css"
+  },
+  {
+    "revision": "a9da008ff1e9cb92c39c",
+    "url": "/static/js/2.db0ca4ec.chunk.js"
+  },
+  {
+    "revision": "ff7912f6c1f7cbdce0ee8d8e782e1649",
+    "url": "/index.html"
+  }
+];
